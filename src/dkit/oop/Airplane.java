4d0f4ef9b0bae7be3f7a0e47ca05b65d4813cf54
kevin <kevin.daly02@outlook.com>
2021-12-15 12:02:14 +0000

    Question 1 and 2

--- src/dkit/oop/Airplane.java
+++ src/dkit/oop/Airplane.java
@@ -5,7 +5,7 @@ public abstract class Airplane {
     private int id;
     private String type;     // e.g. "Airbus 380"
     private static int nextId = 100;  // to generate/supply unique id values
-    
+
     Airplane(String type) {
         this.id = Airplane.nextId++;    // generate unique id
         this.type = type;
